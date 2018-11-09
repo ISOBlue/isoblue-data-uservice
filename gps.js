ac3500a84a71b8d8a4f6893d4b941caf8bf62780
Yang Wang <wang701@purdue.edu>
2018-11-09 20:47:15 +0000

    Fix: a bit of things

--- gps.js
+++ gps.js
@@ -6,7 +6,7 @@ var kafka = require('node-rdkafka');
 var oada = require('@oada/oada-cache').default;
 
 var schema = fs.readFileSync('./schema/gps.avsc')
-var topics = ['debug'];
+var topics = ['remote'];
 var domain = 'https://128.46.71.204';
 var token = 'abc';
 const type = avro.Type.forSchema(JSON.parse(schema));
@@ -50,9 +50,10 @@ return oada.connect(connectionArgs).then((conn) => {
   console.log('OADA connected!');
 
   var consumer = new kafka.KafkaConsumer({
-   'group.id': 'isoblue-gps',
-   'auto.offset.reset': 'latest',
-   'metadata.broker.list': 'cloudradio39.ecn.purdue.edu:9092',
+    'group.id': 'isoblue-gps',
+    'auto.offset.reset': 'latest',
+    'metadata.broker.list': 'cloudradio39.ecn.purdue.edu:9092'
+//   'metadata.broker.list': 'localhost:9092',
   });
 
   consumer.connect()
@@ -62,7 +63,6 @@ return oada.connect(connectionArgs).then((conn) => {
       consumer.consume();
     })
     .on('data', function(data) {
-
       /* disregard any message that does not have heartbeat key */
       var key_split = data.key.toString().split(':')
       if (key_split[0] != 'gps') {
@@ -75,14 +75,26 @@ return oada.connect(connectionArgs).then((conn) => {
       /* setup avro decoder */
       var gps_datum = type.fromBuffer(data.value);
 
+//      console.log(gps_datum.gps.object_name)
+
+      if (gps_datum.gps.object_name === 'TPV') {
+        gps_tpv_datum = gps_datum.gps.object.tpv_record;
+//        console.log(gps_tpv_datum);
+      } else {
+        return;
+      }
+
       /* read each field */
-      var genTime = hb_datum['timestamp'];
-      var lat = hb_datum['lat'];
-      var lng = hb_datum['lon'];
+      var genTime = gps_tpv_datum['time'];
+      var lat = gps_tpv_datum['lat'];
+      var lon = gps_tpv_datum['lon'];
+
+      console.log(genTime, lat, lon);
 
       /* get the day bucket from generation timestamp */
       var date = String(new Date(genTime * 1000).toISOString().slice(0, 10));
-      var hour = String(new Date(genTime * 1000).toTimeString().slice(0, 3)) + '00';
+      var hour = String(new Date(genTime * 1000).toTimeString().slice(0, 3)) +
+        '00';
 
       console.log('date_bucket is:', date, 'hr_bucket is:', hour);
 
@@ -100,15 +112,15 @@ return oada.connect(connectionArgs).then((conn) => {
       console.log(path);
 
       /* do the PUT */
-      
-      conn.put({
-        tree,
-        path,
-        data,
-      }).catch((err) => {
-        console.log(err);
-        throw err;
-      });
+
+//      conn.put({
+//        tree,
+//        path,
+//        data,
+//      }).catch((err) => {
+//        console.log(err);
+//        throw err;
+//      });
     });
 });
 
