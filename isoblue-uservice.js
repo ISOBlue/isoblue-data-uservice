24986a42aa156e79dabf83776e73cc1b1c59f674
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-05-02 22:03:06 -0400

    update rev format

--- isoblue-uservice.js
+++ isoblue-uservice.js
@@ -20,21 +20,21 @@ const kafka_broker = process.env.KAFKA_BROKER;
 var tree = {
   bookmarks: {
     _type: "application/vnd.oada.bookmarks.1+json",
-    _rev: "0-0",
+    _rev: 0,
     isoblue: {
       _type: "application/vnd.oada.isoblue.1+json",
-      _rev: "0-0",
+      _rev: 0,
       "device-index": {
         "*": {
           _type: "application/vnd.oada.isoblue.device.1+json",
-          _rev: "0-0",
+          _rev: 0,
           "*": {
             _type: "application/vnd.oada.isoblue.dataset.1+json",
-            _rev: "0-0",
+            _rev: 0,
             "day-index": {
               "*": {
                 _type: "application/vnd.oada.isoblue.day.1+json",
-                _rev: "0-0",
+                _rev: 0,
                 "hour-index": {
                   "*": {
                     _type: "application/vnd.oada.isoblue.hour.1+json",
